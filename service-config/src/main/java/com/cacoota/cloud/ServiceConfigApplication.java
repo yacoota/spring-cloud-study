b7226c47932c5381064e34a6543b9c4f03f865eb
宇凡 <gao_20022002@126.com>
2020-04-14 16:49:31 +0800

    springcloud知识整理总结--springboot分支-2.X.X

--- service-config/src/main/java/com/cacoota/cloud/ServiceConfigApplication.java
+++ service-config/src/main/java/com/cacoota/cloud/ServiceConfigApplication.java
@@ -1,14 +1,9 @@
 package com.cacoota.cloud;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
